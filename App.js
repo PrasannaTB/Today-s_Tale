b8965f7d5fd7ff7335c463e05ad36ca7ae0aa659
Prasanna Tamang Bomjan <prasannatmg.400@gmail.com>
2024-05-07 09:16:36 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'
+
+function ListScreen() {
+  return(
+    <View style = {styles.container}>
+      <Text>List</Text>
+    </View>
+  );
+}
+
+function CalendarScreen() {
+  return(
+    <View style = {styles.container}>
+      <Text>Calendar</Text>
+    </View>
+  );
+}
+
+function AddScreen() {
+  return(
+    <View style = {styles.container}>
+      <Text>Add</Text>
+    </View>
+  );
+}
+
+function GalleryScreen() {
+  return(
+    <View style = {styles.container}>
+      <Text>Gallery</Text>
+    </View>
+  );
+}
+
+function MeScreen() {
+  return(
+    <View style = {styles.container}>
+      <Text></Text>
+    </View>
+  );
+}
+
+const screenOptions = ({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+
+    if (route.name === 'List') {
+      iconName = 'list';
+    } else if (route.name === 'Calendar') {
+      iconName = 'calendar';
+    } else if (route.name === 'Add') {
+      iconName = 'add';
+    } else if (route.name === 'Gallery') {
+      iconName = 'image';
+    } else if (route.name === 'Me') {
+      iconName = 'person';
+    }
+
+    return <Ionicons name = {iconName} size={size} color={color} />
+  }
+});
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen name = "List" component = {ListScreen} />
+        <Tab.Screen name = "Calendar" component = {CalendarScreen} />
+        <Tab.Screen name = "Add" component = {AddScreen} />
+        <Tab.Screen name = "Gallery" component = {GalleryScreen} />
+        <Tab.Screen name = "Me" component = {MeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
